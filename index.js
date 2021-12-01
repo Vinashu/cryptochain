2a243e24d38a4b0a4ba0b8059ba2b6d75bcf4d82
Rogerio de Leon Pereira <vinashu@gmail.com>
2021-11-30 23:12:33 -0600

    API Mine post end-point

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const BlockChain = require('./blockchain');
 
 const app = express();
 const blockchain = new BlockChain();
 
+app.use(bodyParser.json());
+
 app.get('/api/blocks', (req, res) => {
    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+    blockchain.addBlock({ data });
+    res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
